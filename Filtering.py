d900cb0b37138f3cc5fe0a7d803a12b75d2e0544
FILTERING-RUBIKA-RYSON <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2021-11-26 23:12:57 +0330

    Add files via upload

---
+++ Filtering.py
@@ -0,0 +1,249 @@
+# MAMAD RYSON ☘️ 
+print("MMD RYSON")
+print("")
+print("Hello")
+print(" ")
+print(" ")
+print("Load . . .")                 
+import os
+# AHA
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("      GOD FILTERING")
+print (Fore.YELLOW + "")
+x = """ mohamd Ryson bot running....
+----RYSON.-------RYSON-------RYSON-----RYSON--------RYSON-----RYSON------------RYSON-----RYSON---RYSON---RYSON----RYSOM----------RYSON---RYSON-----RYSON-----RYSON----RYSON----RYSON----RYSON----RYSON---RYSON--------RYSON----!!!!!-------RYSON----RYSON----RYSOM---(MOHAMMED RYSON)--------RYSON--------OSTOREH--FILTERING=-IM--MOHAMMED--RYSON--!!!!!!!
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+
+I Lobe You-
+Manufacturer- ryson
+
+                            RYS0N
+         GOD FILTERING RUBIKA MMD RYSON 
+,-.
+               _,---.          /'|\          __,---,___
+            ,-'    \    -.____,-'  |  -.____,-'    //    -.
+          ,'        |           ~'\     /~           |        .          /      ___//              . ,'          ,  , \ ___      \
+        |    ,-'   -.__   _         |        ,    __,-'   -.    |
+        |   /          /\_     .    |    ,      _/\          \   |
+        \  |           \ \-.___ \   |   / ___,-'/ /           |  /
+         \  \           | ._   \  |  //'   _,' |            /  /
+          -.\         /'  _ ---'' , . ``---' _  \         /,-'
+             `       /     \    ,='/ \=.    /     \       ''
+                     |__   /|\_,--.,-.--,--._/|\   __|
+                     /  ./  \\ |  |  | /,//'   \   \     MMD  ~> RYSON
+                    /   /     ||--+--|--+-/-|     \   \
+                   |   |     /'\_\_\ | /_/_/\     |   |
+                    \   \__, \_     ~'     _/ .__/   /
+                     -._,-'   -._______,-'   -._,-'
+
+                    CODE FILTERI RUBIKA
+             MOHAMD RYSON 💗
+          WEBLOG GHAVANIN ~> Rules-Filtering-Ryson.Blogfa.com Ostoreh Filter/\/\/\
+
+
+FILTER RUBIKA ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||_ _ _ _ _
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# MAMAD*_*
+#Daly
+#          ▄▀▄   I LOVE YOU▄▀▄
+#         All Chaker Ton Mmd 
+#     
+#   
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("Ok mamad ryson . ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA @Conquest_Rayson IM MOHAMD")
+print ("                         ID telegram @Creator_Rayson")
+print ("                          WebLog ghavanin : Immamadryson.Blogfa.com ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("!!")
+time.sleep(0.7)
+print ("!!I")
+time.sleep(0.7)
+print ("!!!M")
+time.sleep(0.6)
+print ("!!!!M")
+time.sleep(0.5)
+print ("!!!!!M")
+time.sleep(0.4)
+print ("!!!!!!Y")
+time.sleep(0.3)
+print ("!!!!!!!S")
+time.sleep(0.2)
+print ("!!!!!!!!O")
+time.sleep(0.1)
+print ("!!!!!!!!!!N")
+time.sleep(0.5)
+print ("!!!!!!!!!!!A")
+time.sleep(0.1)
+print ("!!!!!!!!!!!!M")
+time.sleep(0.5)
+print ("!!!!!!!!!!!!!!")
+time.sleep(0.1)
+print("!!!!!!!!!!!!!!!!")
+time.sleep(0.5)
+print ("!!!!!!!!!!!!!!")
+time.sleep(0.1)
+print ("!!!!!!!!!!!!!")
+time.sleep(0.5)
+print("!!!!!!!!!!!!!")
+time.sleep(1.0)
+print("!!!!!!!!!!!!")
+time.sleep(0.5)
+print("!!!!!!!!!!!")
+time.sleep(0.1)
+print("!!!!!!!!!!")
+time.sleep(0.5)
+print("!!!!!!!!!")
+time.sleep(1.0)
+print("!!!!!!!!")
+time.sleep(0.5)
+print("!!!!!!!")
+time.sleep(1.0)
+print("!!!!!!")
+time.sleep(0.5)
+print("!!!!!")
+time.sleep(1.0)
+print("!!!!")
+time.sleep(0.5)
+print("!!!")
+time.sleep(1.0)
+print("!!")
+time.sleep(0.5)
+print("!")
+print (" ")
+print("")
+# vared kardane addad baraye gereftan kod
+up = input("Code [Chanel] (2) || Code [Grup] (3) || {Code [Account] (4)} > > >>")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Loading . . . .
+. . . . . . .  . . .
+ . . . . . . . . . . . .
+ HA HA HA 💣 💣 💣 💣 💣 💣 💣 💣
+    
+     █████████
+     █▄█████▄█        COD FILTERING-RUBIKA-RYSON | MMD RYSON
+     █▼▼▼▼▼           MMD @Khalegh_Ryson
+     █                Gh: http://Rules-Filtering-Ryson
+     █▲▲▲▲▲
+     █████████
+      ██ ██""")
+
+print(f"{red} ")
+
+
+
+if up == "2":
+
+        print ('code for Channel ≠>' + '%s%s (http://i.Q.fi/h0."c1-atering/) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("Bismillah Cod FILTER")
+
+
+if up == "3":
+        print ('code group ≠>' + '%s%s (/3.2.9.&.dst2ee76RbZP0.py/i.q.w.6.0.3.8//) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "3":
+
+        print("Code Sakhteh Shode Tavasot Ryson")
+
+
+if up == "4":
+
+        print ('code Filter account ≠>' + '%s%s (/6.8.0.2/4.9.8.1/k/g0/j/a/1.8.9.4.2.0.8.6/0.9.1.4.0.1.9.1.8.8.6/) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "4":
+        print("Khalegh Ryson")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal haye rubika MMD RYSON =
+
+@Khalegh_Ryson + @Khalegh_Ryson
+
+@Creator_Rayson + @Creator_Rayson
+ """)
+print (" Kanal rubikam RYSON=>> https://rubika.ir/joinc/JDDGCJF0YETLWSRZLYDCJWIOBMCANFKJ")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Off Mizanam Az Rubika Im Mmd Ryson")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print (' In Ro Befrest Bad Blake Kon' + '%s%s  @-3.4.6  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print ("""code ro ( 3 ) bar bezanin to Sayer
+baraye account Lotfan Shakh Bazi Dar Naiarid Tanks""")
+print(" ")
+print(" ")
+print(" ")
+print ("THE END")
+print("❤️❤️")
+print("MMD RYSON")
+print (Fore.WHITE + "")
