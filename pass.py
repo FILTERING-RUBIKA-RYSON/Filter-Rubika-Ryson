6e06777b300ffc025ddb3ce50b7cd683bf9f8fb3
Ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2021-12-28 13:30:00 +0330

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,44 @@
+import string
+import random
+import string
+import random
+
+
+def password(size=8, chars=string.ascii_letters + string.digits +
+             string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+print(password(int(input(""" 
+
+Code Filtering Acc >> """))))
+print ("")
+import string
+import random
+
+
+def password(size=8, chars=string.ascii_letters + string.digits +
+             string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+print(password(int(input(""" 
+
+Code Filter Chanel >> """))))
+print("")
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz!5788¥€fh--_'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ascii_letters = ascii_lowercase + ascii_uppercase
+import string
+import random
+
+
+def password(size=8, chars=string.ascii_letters + string.digits +
+             string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+print(password(int(input(""" 
+
+Code Filtering Group>> """))))
+print ("")
