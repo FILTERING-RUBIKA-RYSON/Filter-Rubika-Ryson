1d7f66221c69d9234bb6897f9cf10ec1e81605a7
Ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-01-17 16:54:23 +0330

    Update us.py

--- us.py
+++ us.py
@@ -19,15 +19,15 @@ for i in Sdf:
   sleep(0.02)
   print(i,end='',flush=True)
 print()
-print(Fore.RED+" Play Ok ")
+print(Fore.RED+" Rumika's random filtering code please use correctly: OK PlAY FIL ")
 sleep(2.1)
 print()
 def random_string_genera_variable_size(min_size, max_size, allowed_chars):
   return ''.join(random.choice(allowed_chars) for x in range(randint(min_size, max_size)))
 chars = string.ascii_letters + string.punctuation
-print(Fore.GREEN+"    "+"|"+"_"*30+"|")
+print(Fore.GREEN+"    "+"|"+"_"*40+"|")
 print("     "+Fore.YELLOW+'Code Account >> ', random_string_genera_variable_size(6, 24, chars))
-print(Fore.GREEN+"    "+"|"+"_"*30+"|")
+print(Fore.GREEN+"    "+"|"+"_"*40+"|")
 print()
 sleep(1.0)
 print(Fore.GREEN+""" OK """)
