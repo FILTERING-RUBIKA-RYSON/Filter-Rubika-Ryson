81a2812933c79e15e0f94b509f10a2b70ff40584
ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-01-30 20:47:26 +0330

    Update report.py

--- report.py
+++ report.py
@@ -11,17 +11,17 @@ import sys
 print (" User Filtering Vs Reporter _ _ _ _")
     
 soheil = """
-     \033[41m[1]\33[1;0m Group=
-     \033[41m[2]\33[1;0m Account=
-     \033[41m[3]\33[1;0m Chanel=
-     \033[41m[4]\33[1;0m Exit=
+     \033[41m[1]\33[1;0m GROUP-
+     \033[41m[2]\33[1;0m ACCOUNT-
+     \033[41m[3]\33[1;0m CHANEL-
+     \033[41m[4]\33[1;0m EXIT-
 """
 for i in soheil:
 	sleep(0.05)
 	print(i,end='',flush=True)
 print()
 ################   Inputs
-shah = int(input(Fore.CYAN+"     \033[41m[?]\33[1;0m"+"\33[1; Which do you want? >>> \033[93m"+Fore.RED+""))
+shah = int(input(Fore.CYAN+"     \033[41m[?]\33[1;0m"+"\33[1; Type number >>>_ \033[93m"+Fore.RED+""))
 ################################## Grop
 if(str(shah) == "1"):
   os.system("python gp.py")
