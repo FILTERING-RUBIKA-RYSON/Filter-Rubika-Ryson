4fe7b96e030482af5c23b39a7dd4f4e329989c18
ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-03-31 15:55:29 +0430

    Update ch.py

--- ch.py
+++ ch.py
@@ -12,7 +12,7 @@ print(Fore.RED+" "+" Loading....")
 sleep(2.0)
 print("\n")
 print(Fore.GREEN+"""
-    Ryson Code ... 
+    coder..
   """)
 sleep(2.0)
 print("\n"*49)
@@ -21,7 +21,7 @@ print("""
   """)
 Dmo = """
 
-My strongest filtering code
+creater codings
 """
 for i in Dmo:
   sleep(0.02)
@@ -32,7 +32,7 @@ def random_string_generator(str_size, allowed_chars):
   return ''.join(random.choice(allowed_chars) for x in range(str_size))
 chars = string.ascii_letters + string.punctuation
 size = 28
-print(Fore.GREEN+"    "+"["+"_"*50+"]")
-print(Fore.RED+'   Cod FIltering Chanel >> ',random_string_generator(size, chars))
-print(Fore.GREEN+"    "+"["+"_"*50+"]")
+print("_____________")
+print(Fore.RED+'   reporter channel >> ',random_string_generator(size, chars))
+print("_____________")
 input()
