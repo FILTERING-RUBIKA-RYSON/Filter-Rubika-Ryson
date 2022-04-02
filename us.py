f8cb01692ba67f896da008c2e7c2184511456093
ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-04-02 16:56:23 +0430

    Update us.py

--- us.py
+++ us.py
@@ -14,12 +14,12 @@ print("\n")
 sleep(2.0)
 print("\n"*80)
 Sdf = """
- Im Ostoreh """
+ telesm server """
 for i in Sdf:
   sleep(0.02)
   print(i,end='',flush=True)
 print()
-print(Fore.RED+" Rumika's random filtering code please use correctly: OK PlAY FIL ")
+print(Fore.RED+" for you ")
 sleep(2.1)
 print()
 def random_string_genera_variable_size(min_size, max_size, allowed_chars):
@@ -28,6 +28,5 @@ chars = string.ascii_letters + string.punctuation
 
 print("     "+Fore.YELLOW+'reporter account—>> ', random_string_genera_variable_size(6, 24, chars))
 print()
-sleep(1.0)
-print(Fore.GREEN+""" OK """)
-input()
+time.sleep(1)
+print ()
