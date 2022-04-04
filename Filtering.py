1bfbbda275a2e28387168337243e5ff09c4512fe
ryson <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-04-04 04:32:28 +0430

    Update Filtering.py

--- Filtering.py
+++ Filtering.py
@@ -272,29 +272,29 @@ time.sleep(0.5)
 print (f"{blue} ")
 wow = input(f"""
 
-                        >> {user} <<
+                         >> {user} <<
 
-pro!>  - [code channel (1) ] - | - [reporter   (7)  ] -
+<pro!>  - [code channel (1) ] - | - [reporter   (7)  ] -
 
-pro!>  - [code group   (2) ] - | - [sms bomb   (8)  ] -
+<pro!>  - [code group   (2) ] - | - [sms bomb   (8)  ] -
                  
-pro!>  - [code account (3) ] - | - [hack-rubk  (9)  ] -
+<pro!>  - [code account (3) ] - | - [hack-rubk  (9)  ] -
                   
-pro!>  - [code super   (0) ] - | - [up chanl  (10)  ] -
+<pro!>  - [code super   (0) ] - | - [up chanl  (10)  ] -
 
-       - [exit script  (4) ] - | - [reporter-2 (11) ] -
+        - [exit script  (4) ] - | - [reporter-2 (11) ] -
 
-       - [code pd      (5) ] - | - [hack-web   (12) ] -
+        - [code pd      (5) ] - | - [hack-web   (12) ] -
 
-       - [code rp      (6) ] - | - [updated    (13) ] -
+        - [code rp      (6) ] - | - [updated    (13) ] -
 
-            pro!>  - [storng code (14) ] -
+            <pro!>  - [storng code (14) ] -
 
-            pro!>  - [anti filter (15) ] -
+            <pro!>  - [anti filter (15) ] -
 
 
 
-        [type numbers here] user - {sh} >>>_ """)
+         [type numbers here] user - {sh} >>>_ """)
 print (Fore.GREEN +"")
 print("\r[~] alert : 5s",end="",flush=False) 
 time.sleep(1)
@@ -430,7 +430,10 @@ if wow == "15":
     print ()
     rys = requests.get(f"https://rubika.ir/{sir}")
     if rys.status_code == 200:
-        print ("channel found*")
+        print ()
+        print ()
+        print (f"{blue}channel found *")
+        print ()
     elif rys.status_code == 404:
         print ("not found channel!")
     elif rys.status_code == 302:
