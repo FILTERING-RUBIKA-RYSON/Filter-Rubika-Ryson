a9a17c75fd864b60277d97095c6b9e922a8c713e
rubika <94848576+FILTERING-RUBIKA-RYSON@users.noreply.github.com>
2022-05-27 17:14:31 +0430

    Update rep.py

--- rep.py
+++ rep.py
@@ -28,6 +28,9 @@ import requests
 # ///// the proxy ////
 proxy = {"https": "127.0.0.1.8000"}
 # /////////
+
+tm = requests.get("https://api.codebazan.ir/time-date/?td=all").text
+
 try:
     os.system("pkg install tor")
 except:
@@ -44,7 +47,9 @@ m = f"""\n\n{darkblue}
 _____________________
 [power reset account]
 —————————————————————
-
+---------------------
+{yellow}⟨⟨ {blue}{te} {yellow}⟩⟩
+---------------------\n
 {green}
 /////////////////////////////////////////
 //////////{blue}anti filter{green} /////////////
@@ -65,20 +70,25 @@ sleep(1)
 print ()
 # ---------- rubika ---------
 def rubika(phone):
+    te = (datetime.today())
     #rubika api
     ruH = {"Host": "messengerg2c4.iranlms.ir","content-length": "96","accept": "application/json, text/plain, */*","user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36","content-type": "text/plain","origin": "https://web.rubika.ir","sec-fetch-site": "cross-site","sec-fetch-mode": "cors","sec-fetch-dest": "empty","referer": "https://web.rubika.ir/","accept-encoding": "gzip, deflate, br","accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6"}
     ruD = {"api_version":"3","method":"sendCode","data":{"phone_number":phone.split("+")[1],"send_type":"SMS"}}
     try:
         ruR = requests.post("https://messengerg2c4.iranlms.ir/", headers=ruH, json=ruD, proxies=proxy)
         if "OK" in ruR.text:
-            print (f"{pink}[+]elimination filtered")
+            sleep(0.5)
+            print (f"{pink}[+]{blue}elimination filtered-{green}{te}\n")
         else:
-            print (f"{pink}[!] {blue}anti report...")
+            sleep(0.4)
+            print (f"{pink}[!] {blue}anti report-{green}{te}{blue}...\n")
     except:
-        print (f"{pink}[!] {blue}anti report...")
+        sleep(0.5)
+        print (f"{pink}[!] {blue}anti report-{green}{te}{blue}...\n")
 
 def main():
-    phone = str(input(f"""\n\n
+    phone = str(input(f"""\n
+{red}[ {blue}{tm} {red}]\n\n
 {white}
 ~~~~~~~~~~~~~~~~~~~
 |||||||||||||||||||
